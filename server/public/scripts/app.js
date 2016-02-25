15e5d7f2477ecbe8be9f4627fa25da68e3e54dc3
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-02-25 09:16:26 -0600

    routes working and json data

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,1 +1,36 @@
-console.log("working");
+var app = angular.module('computer',['ngRoute']);
+
+app.config(function($routeProvider){
+
+  $routeProvider
+    .when('/', {
+      templateUrl:'pages/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl:'pages/about.html',
+      controller: 'MainCtrl'
+    })
+    .when('/services', {
+      templateUrl:'pages/services.html',
+      controller: 'ServicesCtrl'
+    })
+    .when('/contact', {
+      templateUrl:'pages/contact.html',
+      controller: 'MainCtrl'
+    })
+
+});
+
+app.controller('MainCtrl', ['$scope',function(scope){
+console.log("this is main");
+
+}]);
+
+app.controller('ServicesCtrl', ['$scope', '$http',function($scope, $http){
+
+$http.get('../data/services.json').then(function(res){
+  console.log(res);
+});
+
+}]);
